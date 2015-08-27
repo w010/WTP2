4c255d4e7652ad6fc74325bccd57d8eaf729110f
wolo <wolo.wolski@gmail.com>
2015-08-27 02:11:07 +0200

    makelinks

--- makelinks.bat
+++ makelinks.bat
@@ -3,7 +3,7 @@ mklink /D __CONTENTS 	fileadmin\content\
 mklink /D __IMAGES 	fileadmin\templates\default\images\
 mklink /D __TEMPLATES 	fileadmin\templates\default\
 
-mklink /D typo3_src 	typo3_src-6.2.10
+mklink /D typo3_src 	typo3_src-6.2.14
 mklink /D typo3 		typo3_src\typo3
 mklink index.php 		typo3_src\index.php
 
